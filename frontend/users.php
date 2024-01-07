0f0081abc7902fd9b0222716725be3707b2c00e1
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-01-07 21:01:58 +0100

    rendered everything dynamically on userspage

--- frontend/users.php
+++ frontend/users.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once "header.php"; ?>
 
@@ -5,13 +6,20 @@ include_once "header.php"; ?>
     <div class="container">
         <section class="users">
             <header>
+                <?php
+                include_once "../backend/PHP/env.php";
+                $sql = mysqli_query($conn, "SELECT * FROM members WHERE unique_id = {$_SESSION['unique_id']}"); // get the user details
+                if (mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+                }
+                ?>
                 <div class="content">
-                    <img src="" alt="user_img">
+                    <img src="../backend/PHP/images/<?php echo $row['img'] ?>" alt="user_img">
                     <div class="details">
-                        <span>
-                            Ajay Parajuli
-                        </span>
-                        <p>Offline now</p>
+                        <span><?php
+                                echo $row['name'] . " " . $row['lastname'];
+                                ?></span>
+                        <p><?php echo $row['status']; ?></p>
                     </div>
                 </div>
                 <a href="php/logout.php>" class="logout">Logout</a>
