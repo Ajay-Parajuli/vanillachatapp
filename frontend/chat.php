0a3a30248122814a4a441ab10533902d0ada6508
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-01-07 17:22:29 +0100

    frontend chatbox ui created

--- frontend/chat.php
+++ frontend/chat.php
@@ -1,14 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php
+include_once "header.php"; ?>
 
 <body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="" alt="user_img">
+                <div class="details">
+                    <span>Ajay Parajuli</span>
+                    <p>Active Now</p>
+                </div>
+            </header>
+            <div class="chat-box">
 
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" name="outgoing_id" value="" hidden>
+                <input type="text" name="incoming_id" value="" hidden>
+                <input type="text" name="message" class="input-field" placeholder="Type a message here...">
+                <button><i class="fab fa-telegram-plane"></i></button>
+        </section>
+    </div>
+    <script src="./js/chat.js"></script>
 </body>
 
 </html>
